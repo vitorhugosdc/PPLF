df7583dfb6556debf61a499a56588caf08f98e85
Vitor <vitorvitinho9900@hotmail.com>
2023-12-31 10:41:01 -0300

    adição de uma nova fase, ainda sem terminar

--- trabalho_1.rkt
+++ trabalho_1.rkt
@@ -2,34 +2,43 @@
 
 (require racket/base)
 
-(define labirinto
-  '(("#" "#" "#" "#" "#" "#" "#")
-    ("#" " " "D" " " " " "#" "#")
-    ("#" " " "#" "#" "#" " " "#")
-    ("#" " " "#" " " " " "S" "#")
-    ("#" "#" "#" "#" "#" "#" "#")))
+; Definição dos labirintos para cada fase
+(define labirintos
+  (list
+    '(("#" "#" "#" "#" "#" "#" "#")
+      ("#" " " "D" " " " " "#" "#")
+      ("#" " " "#" "#" " " " " "#")
+      ("#" " " "#" " " " " "S" "#")
+      ("#" "#" "#" "#" "#" "#" "#"))
+    '(("#" "#" "#" "#" "#" "#" "#")
+      ("#" " " "D" " " "#" " " "#")
+      ("#" "#" "#" " " "#" " " "#")
+      ("#" " " " " " " "S" " " "#")
+      ("#" "#" "#" "#" "#" "#" "#"))))
 
-(define posição-inicial '(1 1))
-(define posição-saída '(3 5))
+; Definição das posições iniciais e de saída para cada fase
+(define posições-iniciais '((1 1) (1 1)))
+(define posições-saída '((3 5) (3 5)))
 
-; Desafio de programação simples
+; Desafio de programação simples (pode ser ajustado para cada fase)
 (define desafio-posição '(1 2))
-
 (define desafio-resolvido #f)
 
+; Função para o desafio de programação
 (define (desafio-soma)
   (display "Desafio: Insira o corpo de uma função que some dois números.\n")
   (display "Por exemplo, você pode digitar (+ x y).\n")
   (let* ([resposta (read)]
          [contexto (make-base-namespace)]
-         [função-soma (eval `(lambda (x y) ,resposta) contexto)]) ; Avalia no contexto adequado
-    (let ([resultado (função-soma 2 3)]) ; Testa a função
+         [função-soma (eval `(lambda (x y) ,resposta) contexto)])
+    (let ([resultado (função-soma 2 3)])
       (if (= resultado 5)
           (begin
             (set! desafio-resolvido #t)
             (display "Desafio resolvido! Você pode continuar.\n"))
           (display "Resposta incorreta. Tente novamente.\n")))))
 
+; Função para mostrar o labirinto
 (define (mostrar-labirinto labirinto posição)
   (for ([linha (in-range (length labirinto))])
     (for ([coluna (in-range (length (list-ref labirinto 0)))])
@@ -41,13 +50,14 @@
       (display " "))
     (newline)))
 
+; Função para mover o jogador no labirinto
 (define (mover labirinto posição direção)
   (let* ([linha (first posição)]
          [coluna (second posição)]
          [nova-posição
           (cond
             [(equal? direção 'cima) (list (- linha 1) coluna)]
-            [(equal? direção 'baixo)   (list (+ linha 1) coluna)]
+            [(equal? direção 'baixo) (list (+ linha 1) coluna)]
             [(equal? direção 'direita) (list linha (+ coluna 1))]
             [(equal? direção 'esquerda) (list linha (- coluna 1))])]
          [nova-linha (first nova-posição)]
@@ -58,23 +68,36 @@
         nova-posição
         posição)))
 
-(define (jogar)
+; Função principal para jogar uma fase
+(define (jogar-fase índice-fase)
+  (define labirinto-atual (list-ref labirintos índice-fase))
+  (define posição-inicial (list-ref posições-iniciais índice-fase))
+  (define posição-saída (list-ref posições-saída índice-fase))
   (define posição-atual posição-inicial)
+
   (display "Bem-vindo ao Labirinto de Racket! Encontre a saída e resolva os desafios.\n")
-  (let loop ([posição posição-atual])
+
+  (define (loop posição)
     (cond
       [(equal? posição posição-saída)
        (if desafio-resolvido
-           (display "Parabéns! Você encontrou a saída e resolveu os desafios!\n")
+           (if (< índice-fase (sub1 (length labirintos)))
+               (begin
+                 (set! desafio-resolvido #f)
+                 (jogar-fase (add1 índice-fase)))
+               (display "Parabéns! Você encontrou a saída e resolveu os desafios de todas as fases!\n"))
            (display "Você encontrou a saída, mas ainda há desafios a resolver.\n"))]
       [else
-       (mostrar-labirinto labirinto posição)
+       (mostrar-labirinto labirinto-atual posição)
        (when (and (equal? posição desafio-posição) (not desafio-resolvido))
          (desafio-soma))
        (let ([movimento (read)])
          (cond
            [(equal? movimento 'sair) (display "Jogo encerrado.\n")]
            [else
-            (loop (mover labirinto posição movimento))]))])))
+            (loop (mover labirinto-atual posição movimento))]))]))
 
-(jogar)
+  (loop posição-inicial))
+
+; Iniciar o jogo na primeira fase
+(jogar-fase 0)
